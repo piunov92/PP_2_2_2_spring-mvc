809261e165f01077d65b78b10eb5035ee0f6dbd5
Sergey Piunov <piunov92@gmail.com>
2023-05-19 00:27:00 +0300

    delete CarDao, CarDaoImp and update Controller, Service

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,16 +1,21 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import web.service.CarServiceImp;
 
 @Controller
 public class CarsController {
 
-    CarService carService = new CarServiceImp();
+    private static CarService carService;
+
+    @Autowired
+    public CarsController(CarService carService) {
+        CarsController.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
     public String testController(@RequestParam(value = "count", defaultValue = "5") Integer count, ModelMap model) {
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,23 +1,33 @@
 package web.service;
 
-import web.dao.CarDao;
-import web.dao.CarDaoImp;
+import org.springframework.stereotype.Service;
 import web.model.Car;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarServiceImp implements CarService {
 
-    CarDao dao = new CarDaoImp();
+    List<Car> cars = List.of(
+            new Car("Car1", "2001", "1"),
+            new Car("Car2", "2002", "2"),
+            new Car("Car3", "2003", "3"),
+            new Car("Car4", "2004", "4"),
+            new Car("Car5", "2005", "5")
+    );
 
     @Override
     public List<Car> getCountByCarList(int count) {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("Car1", "2001", "1"));
-        cars.add(new Car("Car2", "2002", "2"));
-        cars.add(new Car("Car3", "2003", "3"));
-        cars.add(new Car("Car4", "2004", "4"));
-        cars.add(new Car("Car5", "2005", "5"));
-        return dao.getCountByCarList(cars, count);
+        List<Car> carsByCount = new ArrayList<>();
+        if (count < 5) {
+            int i = 0;
+            while ((count--) > 0) {
+                carsByCount.add(cars.get(i++));
+            }
+        } else {
+            return cars;
+        }
+        return carsByCount;
     }
 }
